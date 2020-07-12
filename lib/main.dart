b8bd5dc5c91482d42334a73256cafc2d0d0e227f
diego wenndson <diego.wenndson@gmail.com>
2020-07-12 19:13:01 -0300

    continuando o desenvolvimento do flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MeuApp());
 
+// ignore: must_be_immutable
 class MeuApp extends StatelessWidget {
+  var textStyle =
+      TextStyle(fontSize: 30, color: Colors.green, fontWeight: FontWeight.bold);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,37 +16,47 @@ class MeuApp extends StatelessWidget {
           ),
           body: Column(
             children: <Widget>[
-              Row(
-                children: [
-                  Name()
-                ],
+              Name(
+                title: 'Texto 1',
+                onPress: () {
+                  print('clicou no item 1');
+                },
               ),
-              Row(
-                children: [
-                  Name(),
-                ]
+              Name(
+                title: 'Texto 2',
+                onPress: () {
+                  print('clicou no item 2');
+                },
               )
             ],
-          )
-      )
+          )),
     );
   }
 }
 
 class Name extends StatelessWidget {
+  Name({
+    //required obriga sempre passar os valores para os parametros
+    @required this.title,
+    @required this.onPress});
+
+  var title;
+  Function onPress;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 200,
-      color: Colors.red,
+      color: Colors.red[200],
       padding: EdgeInsets.all(10),
       margin: EdgeInsets.all(10),
       child: Column(
         children: [
-          Text('Texto de Exemplo'),
+          Text(this.title),
           FlatButton(
+            color: Colors.green,
             child: Text('Click Aqui'),
-            
+            onPressed: this.onPress,
           )
         ],
       ),
